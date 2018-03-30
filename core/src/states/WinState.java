7dfc3a4a0039833599de99ed1f94d6b40c7b4e64
Nora Tomas <noratomas@hotmail.com>
2018-03-30 14:56:46 +0200

    created state for winscreen

---
+++ core/src/states/WinState.java
@@ -0,0 +1,30 @@
+package states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class WinState extends State{
+
+    protected WinState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
